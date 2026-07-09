c2654159c9579f419e3bf39dd29321add030e4d5
Alex Brown <brownal7@uk.ibm.com>
2026-07-09 12:39:44 +0100

    Image search?

--- computer/src/search.js
+++ computer/src/search.js
@@ -4,18 +4,15 @@ search.addEventListener("focus", (event) => {
     console.log("focused")
     search.classList.add("focus")
 })
-search.addEventListener("focusout", (event) =>  {
+search.addEventListener("focusout", (event) => {
     console.log("unfocused")
     search.classList.remove("focus")
 })
 
 function run_search(form) {
     console.log(form);
-    query=search.value;
-    fetch(`http://duckduckgo.com/?q=${query}`, {
-        method: "POST",
-        body: JSON.stringify({q: query})
-    })
+    query = search.value;
+    fetch(`https://duckduckgo.com/?q=${query}`)
         .then(response => {
             if (!response.ok) {
                 throw Error(response)
@@ -23,7 +20,30 @@ function run_search(form) {
 
             return response.text
         })
-        .then(text => console.info(text))
+        .then(text => {
+            const re = /vqd=([\d-]+)\&/
+            const vqd = re.exec(text);
+            return vqd
+        })
+        .then(vqd => {
+            console.log(vqd);
+            return await fetch(`https://duckduckgo.com/i.js?l=en-us&o=json&q=${query}&vqd=${vqd}&f=,,,&p=1&v7exp=a`, {
+                headers: {
+                    'authority': 'duckduckgo.com',
+                    'accept': 'application/json, text/javascript, */*; q=0.01',
+                    'sec-fetch-dest': 'empty',
+                    'x-requested-with': 'XMLHttpRequest',
+                    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+                    'sec-fetch-site': 'same-origin',
+                    'sec-fetch-mode': 'cors',
+                    'referer': 'https://duckduckgo.com/',
+                    'accept-language': 'en-US,en;q=0.9',
+                }
+            })
+        })
+        .then(response => {
+            console.log(response)
+        })
         .catch((error) => console.error(error))
     return false;
 }
